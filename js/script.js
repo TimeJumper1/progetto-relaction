5541391d779d8c4be70c6c8feb398a5a670ef17b
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-04-20 19:07:51 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$('.slider').slick({
+    prevArrow:'<span class="chevron prev"><i class="fas fa-chevron-left"></i></span>',
+    nextArrow:'<span class="chevron next"><i class="fas fa-chevron-right"></i></span>'
+});
